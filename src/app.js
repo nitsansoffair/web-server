178ad548cc3d4c57e5a36f6614fbe5767c6ec7b9
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-28 22:37:49 +0200

    Express

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ app.get('', (req, res) => {
     res.send('Hello express!');
 });
 
+app.get('/help', (req, res) => {
+    res.send('Help page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>title about</h1>.');
+});
+
+app.get('/weather', (req, res) => {
+    res.send('<h1>title weather</h1>.');
+});
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
 });
