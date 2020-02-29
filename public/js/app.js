d9cf3ce37b05112b72670cc66eca9f091f75e550
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-29 18:58:36 +0200

    Writing up the User Interface

--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,28 @@ console.log('Client side javascript file is loaded!');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
 
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
     fetch('http://localhost:3000/weather?address=' + location)
         .then((res) => {
             res.json().then((data) => {
                 if(data.error){
-                    console.log(data.error);
+                    messageOne.textContent = data.error;
                 }else{
-                    console.log(data.location);
-                    console.log(data.forecast);
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.forecast;
                 }
             });
         })
         .catch((err) => {
-            console.log(err);
+            messageOne.textContent = err;
         });
 });
