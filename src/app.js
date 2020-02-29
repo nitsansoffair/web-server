be4170bd7de2a4216a9d296b673b7e3076081f89
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-29 18:08:31 +0200

    Weather

--- src/app.js
+++ src/app.js
@@ -35,9 +35,16 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address!'
+        });
+    }
+
     res.send({
         forecast: 'string',
-        location: 'string'
+        location: 'string',
+        address: req.query.address
     });
 });
 
